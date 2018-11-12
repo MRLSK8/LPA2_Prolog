c7619ba1ba5aef677965e72318b07259211a6e3d
Marcelo Lima <marcelo_sp5@hotmail.com>
2018-11-12 18:43:14 -0300

    Exercicio

---
+++ Exercico-IP2.pl
@@ -0,0 +1,43 @@
+continente(asia).
+continente(america).
+continente(europa).
+pais(portugal, europa, 10).
+pais(espanha, europa, 48).
+pais(franca, europa, 52).
+pais(belgica, europa, 9).
+pais(alemanha, europa, 82).
+pais(holanda, europa, 15).
+pais(eua, america, 235).
+pais(brasil, america, 155).
+pais(china, asia, 1100).
+pais(mongolia, asia, 3).
+fronteira(portugal, espanha).
+fronteira(franca, espanha).
+fronteira(franca,belgica).
+fronteira(belgica, alemanha).
+fronteira(belgica, holanda).
+fronteira(alemanha, holanda).
+fronteira(alemanha, franca).
+fronteira(china, mongolia).
+
+% junto(P1,P2) que sucede se o pais P1 faz fronteira com P2.
+junto(P1, P2):- fronteira(P1 , P2) ; fronteira(P2, P1).
+
+% paises_continente(Lista, Cont) que calcula a Lista de países existentes num dado continente.
+paises_continente(List, Cont):- findall(Country, pais(Country,Cont,_), List).
+
+% paises_grandes(Lista, Cont) que calcula a Lista de países com mais de 100 milhões de habitantes de um dado continente.
+paises_grandes(Lista, Cont):- findall(Country,(pais(Country, Cont, X), X > 100), Lista).
+
+% dois_mais_pop(P1, P2), que calcula quais os dois países com mais habitantes.
+%dois_mais_pop(P1,P2):- findall(Country, pais(Country, _, X).
+
+% descricao(List), que retorne uma lista com a descrição de todos os países, incluindo o seu nome, continente e população
+% [portugal*europa*10, …,china*asia*1000].
+descricao(List):- findall(Pais*Cont*Pop , pais(Pais, Cont, Pop), List).
+
+% chego_la_facil(P1, P2) que sucede se é possível chegar de P1 a P2,
+% directamente ou atravessando unicamente um outro país.
+chego_la_facil(P1, P2):- (fronteira(P1, P2); fronteira(P2, P1));(fronteira(X, P1);fronteira(P1, X)),(fronteira(X, P2); fronteira(P2, X)), P1\=P2.
+
+
